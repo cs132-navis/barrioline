9aa2aa92cc4a45d1a299bdc15c6100d9103800a5
NAVIS <161105750+cs132-navis@users.noreply.github.com>
2024-06-19 20:07:40 +0800

    Updated ML, conc, reco

--- script.js
+++ script.js
@@ -40,3 +40,21 @@ menuIcon.onclick = () => {
   }
     x[slideIndex-1].style.display = "block";  
 }
+
+var MLslideIndex = 1;
+showDivs2(MLslideIndex);
+
+function plusDivs2(n) {
+    showDivs2(MLslideIndex += n);
+}
+
+function showDivs2(n) {
+  var i;
+    var x = document.getElementsByClassName("MLslides");
+  if (n > x.length) {MLslideIndex = 1}
+    if (n < 1) {MLslideIndex = x.length}
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";  
+  }
+    x[MLslideIndex-1].style.display = "block";  
+}
